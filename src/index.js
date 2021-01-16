05ae66a44dcc72f0dc1c97173991ecf826b2a062
Sidd107 <44132848+Sidd107@users.noreply.github.com>
2021-01-17 03:58:59 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(
+  <div>
+    <h1>Career:</h1>
+    <ul>
+      <li>Complete Btech</li>
+      <li>Pursue good job</li>
+      <li>Get a good life</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
